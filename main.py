c73f2c282492a0eac30dddd243c7aaea74406a0a
RowarthMatt <rowarthmatt@gmail.com>
2025-06-10 08:52:01 +0100

    Ch1 L6

--- main.py
+++ main.py
@@ -1,16 +1,37 @@
 import os
+import sys
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
 
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
 client = genai.Client(api_key=api_key)
 
-response = client.models.generate_content(model="gemini-2.0-flash-001",contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.")
-print(response.text)
+if len(sys.argv) > 1:
+    content = sys.argv[1]
+else:
+    print("Prompt required")
+    sys.exit(1)
+
+verbose = False
+if len(sys.argv) > 2:
+    if sys.argv[2] == "--verbose":
+        verbose = True
+
+messages = [
+    types.Content(role="user", parts=[types.Part(text=content)]),
+]
+
+response = client.models.generate_content(model="gemini-2.0-flash-001",contents=messages)
 
 promt_tokens = response.usage_metadata.prompt_token_count
 resposne_tokens = response.usage_metadata.candidates_token_count
 
-print(f"Prompt tokens: {promt_tokens}")
-print(f"Response tokens: {resposne_tokens}")
+if verbose:
+    print(f"User prompt: {content}")
+    print(response.text)
+    print(f"Prompt tokens: {promt_tokens}")
+    print(f"Response tokens: {resposne_tokens}")
+else:
+    print(response.text)
